7bab0383559408378566f5f6ae737836d0318c7a
AlexBuenoL <abuenoleon@gmail.com>
2026-03-15 20:39:35 +0100

    wereable and images ingestion into minio

--- src/landing_zone/clients/tcia_client.py
+++ src/landing_zone/clients/tcia_client.py
@@ -1,89 +1,36 @@
 import os
-import boto3
 import pandas as pd
 import requests
 import io
-from botocore.exceptions import ClientError
 from tcia_utils import nbia
 
-# --- Configuration ---
-MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://minio:9000")
-ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minio_admin")
-SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minio123456")
-BUCKET_NAME = "landing-zone"
 
-# URL for expert-labeled annotations (The "Ground Truth")
-ANNOTATION_URL = "https://www.cancerimagingarchive.net/wp-content/uploads/CPTAC-CCRCC-Annotation-Metadata.csv"
+ANNOTATION_URL = os.getenv("TCIA_API_URL")
 
-def get_minio_client():
-    return boto3.client("s3", endpoint_url=MINIO_ENDPOINT,
-                         aws_access_key_id=ACCESS_KEY,
-                         aws_secret_access_key=SECRET_KEY)
 
-def ensure_bucket_exists(s3_client, bucket_name):
-    try:
-        s3_client.head_bucket(Bucket=bucket_name)
-    except ClientError:
-        s3_client.create_bucket(Bucket=bucket_name)
-
-def ingest_balanced_data(total_series_limit=20):
+def get_balanced_metadata(total_series_limit: int = 20) -> pd.DataFrame:
     """
-    Downloads a balanced set of Tumor vs. Negative series and uploads to MinIO.
+    Fetches annotations and returns a balanced df of tumor and negative samples.
     """
-    s3 = get_minio_client()
-    ensure_bucket_exists(s3, BUCKET_NAME)
-    
-    # 1. Fetch and Parse Metadata Labels
-    print("Fetching expert annotations for class balancing...")
     response = requests.get(ANNOTATION_URL)
+    response.raise_for_status()
     df = pd.read_csv(io.StringIO(response.text))
     
-    # Define classes based on CPTAC-CCRCC annotation types
-    # Class 1: Tumor Segmentation (Tumor present)
-    # Class 0: Negative Assessment (No tumor)
+    # Define classes
     tumor_series = df[df['AnnotationType'] == 'Tumor Segmentation']
     negative_series = df[df['AnnotationType'] == 'Negative Assessment']
     
-    # 2. Perform Balanced Sampling
+    # Balanced sampling
     n_per_class = total_series_limit // 2
     sampled_tumor = tumor_series.sample(n=min(n_per_class, len(tumor_series)))
     sampled_negative = negative_series.sample(n=min(n_per_class, len(negative_series)))
     
-    balanced_df = pd.concat([sampled_tumor, sampled_negative])
-    print(f"Balanced Dataset Plan: {len(sampled_tumor)} Tumor, {len(sampled_negative)} Negative.")
+    return pd.concat([sampled_tumor, sampled_negative])
 
-    # 3. Ingest the Labels (Mapping File) to MinIO
-    # This satisfies the requirement for semi-structured data management
-    label_csv = balanced_df[['SeriesInstanceUID', 'AnnotationType', 'Subject ID']]
-    s3.put_object(
-        Bucket=BUCKET_NAME,
-        Key="metadata/labels/cptac_balanced_labels.csv",
-        Body=label_csv.to_csv(index=False)
-    )
 
-    # 4. Download and Ingest Images
-    download_path = "./temp_balanced_images"
-    for _, row in balanced_df.iterrows():
-        series_id = row['SeriesInstanceUID']
-        label = "tumor" if row['AnnotationType'] == 'Tumor Segmentation' else "negative"
-        
-        print(f"Ingesting Series: {series_id} (Class: {label})")
-        # Download specific series
-        nbia.downloadSeries(series_id, path=download_path, format="zip")
-        
-        # Upload files to Landing Zone with labeled paths
-        for root, _, files in os.walk(download_path):
-            for file in files:
-                if file.endswith(".dcm"):
-                    local_file = os.path.join(root, file)
-                    # DataOps Tip: Partitioning by class simplifies P2 processing
-                    object_name = f"unstructured/cptac/{label}/{file}"
-                    s3.upload_file(local_file, BUCKET_NAME, object_name)
-                    os.remove(local_file) # Clean up to save container space
-    
-    print("Ingestion complete.")
-
-if __name__ == "__main__":
-    # For 'Big Data' simulation in a dev environment, 20 series is a good start
-    # Each series has many images, resulting in ~2000-4000 total images.
-    ingest_balanced_data(total_series_limit=20)
+def download_dicom_series(series_id: str, download_path: str):
+    """
+    Downloads a specific series from TCIA to a local temporary directory.
+    """
+    # nbia.downloadSeries makes the API call and zip extraction locally
+    nbia.downloadSeries(series_id, path=download_path, format="zip")
--- src/landing_zone/landing_zone.py
+++ src/landing_zone/landing_zone.py
@@ -1,9 +1,13 @@
 import os
+import time
+import shutil
 from datetime import datetime, timezone
 
 from src.landing_zone.minio_manager import MinIOManager
 from src.landing_zone.clients.fhir_client import get_patients, get_conditions_for_patient, get_raw_xml
 from src.landing_zone.clients.weather_client import get_weather
+from src.landing_zone.clients.tcia_client import get_balanced_metadata, download_dicom_series
+from src.landing_zone.clients.wearable_client import list_devices, get_reading
 
 """
 IMPORTANT:
@@ -11,7 +15,10 @@ Now we have all paths (cold, warm, hot) in a single script.
 Should we separate the script into the different data sources to manage the different ingestion frequencies?
 For example:
 - One script to ingest wereable data (Hot-Path) to be executed every minute with Airflow
+    -> do it even just with the landing zone?
 - Other script to ingest weather data (Warm-Path) to be executed daily with Airflow
+
+We also have to manage stream data with Kafka
 """
 
 def ingest_medical_histories(
@@ -19,7 +26,7 @@ def ingest_medical_histories(
         bucket: str,
         fhir_patients_path: str,
         fhir_conditions_path: str,
-        fhir_delta_path: str
+        delta_table_name: str
     ):
     """
     Ingests FHIR patient and condition data into the Landing Zone.
@@ -70,13 +77,13 @@ def ingest_medical_histories(
     # Save as Delta Lake
     if all_structured_data:
         print(f"Updating Delta Table with {len(all_structured_data)} records...")
-        minio_manager.save_as_delta(bucket, fhir_delta_path, all_structured_data)
+        minio_manager.save_as_delta(bucket, delta_table_name, all_structured_data)
 
 
 def ingest_weather_data(
         minio_manager: MinIOManager,
         bucket: str,
-        weather_path: str
+        delta_table_name: str
     ):
     """
     Ingests weather data for patient locations.
@@ -123,17 +130,112 @@ def ingest_weather_data(
         print(f"Ingesting {len(weather_batch)} weather records into Delta Lake...")
         minio_manager.save_as_delta(
             bucket=bucket,
-            table_name=weather_path,
+            table_name=delta_table_name,
             data=weather_batch
         )
 
 
-def ingest_medical_images(minio_manager: MinIOManager):
-    pass
+def ingest_medical_images(
+        minio_manager: MinIOManager, 
+        bucket: str,
+        temp_dir: str,
+        total_series: int = 10
+    ):
+    """
+    Orchestrates the retrieval of balanced imaging data and its storage in MinIO.
+    Path: Cold Path (Batch)
+    Format: Unstructured (DICOM) and Semi-Structured (CSV Labels)
+    """
+    
+    print(f"Starting ingestion of {total_series} medical image series...")
+
+    # Get balanced metadata (image labels)
+    balanced_df = get_balanced_metadata(total_series_limit=total_series)
+    
+    # Store labels as mapping file (semi-structured data)
+    labels_csv = balanced_df[['SeriesInstanceUID', 'AnnotationType', 'Subject ID']].to_csv(index=False)
+    minio_manager.save_object(
+        bucket=bucket,
+        key="metadata/labels/cptac_balanced_labels.csv",
+        data=labels_csv.encode('utf-8')
+    )
+
+    # Iterate through series and ingest images (unstructured data)
+    for _, row in balanced_df.iterrows():
+        series_id = row['SeriesInstanceUID']
+        label = "tumor" if row['AnnotationType'] == 'Tumor Segmentation' else "negative"
+        
+        print(f"  Processing series {series_id} (Class: {label})")
+        
+        # Download locally with the client
+        download_dicom_series(series_id, temp_dir)
+        
+        # Upload to MinIO partitioned by class
+        for root, _, files in os.walk(temp_dir):
+            for file in files:
+                if file.endswith(".dcm"):
+                    local_path = os.path.join(root, file)
+                    with open(local_path, 'rb') as f:
+                        object_key = f"images/{label}/{file}"
+                        minio_manager.save_object(bucket, object_key, f.read())
+        
+        # Clean local temp files after each series
+        if os.path.exists(temp_dir):
+            shutil.rmtree(temp_dir)
+
+    print("Medical image ingestion completed successfully.")
 
 
-def ingest_wearable_data(minio_manager: MinIOManager):
-    pass
+def ingest_wearable_data(
+        minio_manager: MinIOManager, 
+        bucket: str,
+        delta_table_name: str,
+        continuous: bool = False, 
+        interval: int = 10,
+    ):
+    """
+    Ingests health metrics from wearable devices.
+    Path: Hot Path (Streaming)
+    Format: Structured (Delta Lake)
+    """
+
+    print(f"Starting wearable ingestion (Mode: {'Streaming' if continuous else 'Batch'})")
+
+    def run_ingestion_cycle():
+        try:
+            # Get active devices
+            devices = list_devices()
+            current_readings = []
+            
+            # Get current snapshot for all devices
+            for dev_id in devices:
+                try:
+                    reading = get_reading(dev_id)
+                    reading["ingested_at"] = datetime.now(timezone.utc).isoformat()
+                    current_readings.append(reading)
+                except Exception as e:
+                    print(f"Error fetching reading for device {dev_id}: {e}")
+
+            # Add to Delta Table
+            if current_readings:
+                minio_manager.save_as_delta(
+                    bucket=bucket,
+                    table_name=delta_table_name,
+                    data=current_readings
+                )
+                print(f" Successfully collected {len(current_readings)} wearable data.")
+                
+        except Exception as e:
+            print(f"Error in wearable ingestion cycle: {e}")
+
+    # Execution logic
+    if continuous:
+        print(f"Hot Path active. Polling every {interval} seconds... (Ctrl+C to stop)")
+        while True:
+            run_ingestion_cycle()
+            time.sleep(interval)
+    else:
+        run_ingestion_cycle()
 
 
 if __name__ == '__main__':
@@ -146,16 +248,28 @@ if __name__ == '__main__':
         bucket=os.getenv("LANDING_ZONE_BUCKET"),
         fhir_patients_path=os.getenv("FHIR_PATIENTS"),
         fhir_conditions_path=os.getenv("FHIR_CONDITIONS"),
-        fhir_delta_path=os.getenv("FHIR_DELTA")
+        delta_table_name=os.getenv("FHIR_DELTA")
     )
 
     # Ingest weather data
     ingest_weather_data(
         minio_manager=minio_manager,
         bucket=os.getenv("LANDING_ZONE_BUCKET"),
-        weather_path=os.getenv("WEATHER_DELTA")
+        delta_table_name=os.getenv("WEATHER_DELTA")
     )
 
     # Ingest medical images 
+    ingest_medical_images(
+        minio_manager=minio_manager,
+        bucket=os.getenv("LANDING_ZONE_BUCKET"),
+        temp_dir=os.getenv("TMP_IMG_PATH"),
+        total_series=10
+    )
 
     # Ingest wereable data
+    ingest_wearable_data(
+        minio_manager=minio_manager,
+        bucket=os.getenv("LANDING_ZONE_BUCKET"),
+        delta_table_name=os.getenv("WEREABLE_DELTA"),
+        continuous=False
+    )
